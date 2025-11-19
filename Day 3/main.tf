d5e101db23631c055b4230fad0aea07caeb8db0f
SupriyaBarad <supriyabarad5@gmail.com>
2025-11-19 23:32:06 +0530

    autoscaling

--- Day 3/main.tf
+++ Day 3/main.tf
@@ -8,13 +8,13 @@ resource "aws_launch_template" "launch_template_home" {
     image_id = var.image_id
     instance_type = var.instance_type
     vpc_security_group_ids = var.security_group_ids
-    user_data = <<EOF
+    user_data = <<EOT
     #!/bin/bash
     apt update -y
     apt install apache2 -y
     systemctl start apache2
     echo "Hello World" > /var/www/html/index.html
-    EOF
+    EOT
 
     key_name = var.key_pair
     tags = {
@@ -23,3 +23,72 @@ resource "aws_launch_template" "launch_template_home" {
 }
 
 
+resource "aws_launch_template" "launch_template_cloth" {
+    name = "launch-template-cloth"
+    image_id = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_ids
+    user_data = <<EOT
+    #!/bin/bash
+    apt update -y
+    apt install apache2 -y
+    systemctl start apache2
+    mkdir /var/www/html/cloth
+    echo "Hello from Cloth" > /var/www/html/cloth/index.html
+    EOT
+
+    key_name = var.key_pair
+    tags = {
+        env = var.env
+    }
+}
+
+
+resource "aws_launch_template" "launch_template_laptop" {
+    name = "launch-template-laptop"
+    image_id = var.image_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_ids
+    user_data = <<EOT
+    #!/bin/bash
+    apt update -y
+    apt install apache2 -y
+    systemctl start apache2
+    mkdir /var/www/html/laptop
+    echo "Hello from Laptop" > /var/www/html/laptop/index.html
+    EOT
+
+    key_name = var.key_pair
+    tags = {
+        env = var.env
+    }
+}
+
+
+
+resource "aws_autoscaling_group" "asg-home" {
+    name = "asg-home"
+    min_size = var.min_size
+    max_size = var.max_size
+    desired_capacity = var.desired_capacity
+    launch_template = {
+        id = aws_launch_template.launch_template_home.id
+    }
+    availability_zones = var.availability_zones
+    tag {
+        env = var.env
+    }
+
+}
+
+resource "aws_autoscaling_policy""asp-home"{
+    name = "asp-home"
+    autoscaling_group_name = ws_autoscaling_group.asg-home.name
+    policy_type = "TargetTrackingScaling"
+    target_tracking_configuration {
+        predefined_metric_type {
+            predefined_metric_type = "CPUUtilization"
+        }
+        target_value = 50
+    }
+}
