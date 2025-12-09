a410e6998783431acfb178624350bcf1671a29e3
Supriya <supriyabarad5@gmail.com>
2025-12-09 12:24:05 +0530

    cidr error in main.tf

--- Day 4/main.tf
+++ Day 4/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 module "new_vpc" {
     source = "./modules/vpc"
-    vpc_cidr = "172.16.0.0./16"
-    pri_sub_cidr = "172.16.0.0./20"
-    pub_sub_cidr = "172.16.16.0./20"
+    vpc_cidr = "172.16.0.0/16"
+    pri_sub_cidr = "172.16.0.0/20"
+    pub_sub_cidr = "172.16.16.0/20"
 }
 module "instance"{
     source = "./modules/ec2"
